92453a13f760f25cbec133cee0f5d8d39aca2dc5
johnl11235 <31864579+johnl11235@users.noreply.github.com>
2021-12-20 15:07:20 -0500

    XGBoost hyperparameters updated
    Added hyperparamaters for XGBoost configuration, included one minor bugfix.

--- classification/model_XGBoost.py
+++ classification/model_XGBoost.py
@@ -8,16 +8,20 @@ feature_extraction    = { 'tagged_trait'               : True
                         , 'standardized_label_classes' : 5
                         , 'decorrelate'                : 0.6
                         }
-search_grid_options   = { 'n_estimators'      : [50, 100, 150, 200]
-                        , 'learning_rate'     : [0.01, 0.1, 0.2, 0.3]
-                        , 'max_depth'         : range(3, 10)
-#                        , 'colsample_bytree'  : [ i/10.0 for i in range(1, 3) ]
-#                        , 'gamma'             : [ i/10.0 for i in range(3) ]
-                        , 'objective'         : ['multi:softprob']
-                        , 'use_label_encoder' : [False]
-                        , 'num_class'         : [5]
+search_grid_options   = { 'n_estimators'     : [50, 100, 150, 200]
+                        , 'learning_rate'    : [0.01, 0.1, 0.2, 0.3]
+                        , 'max_depth'        : range(3, 10)
+                        , 'colsample_bytree' : [ i/10.0 for i in range(1, 3) ]
+                        , 'gamma'            : [ i/10.0 for i in range(3) ]
+                        , 'objective'        : ['multi:softprob']
                         }
 
-hyperparameter_values = None
+hyperparameter_values = { 'colsample_bytree' : 0.2
+                        , 'gamma'            : 0.2
+                        , 'learning_rate'    : 0.2
+                        , 'max_depth'        : 3
+                        , 'n_estimators'     : 50
+                        , 'objective'        : 'multi:softprob'
+                        }
 
-model_evaluation("XG Boost", xgb.XGBClassifier(objective="multi:softprob", random_state=42), dataset_params=feature_extraction, param_grid=search_grid_options, best_hyperparameters=hyperparameter_values)
+model_evaluation("XG Boost", xgb.XGBClassifier(objective="multi:softprob", eval_metric="mlogloss", random_state=42), dataset_params=feature_extraction, param_grid=search_grid_options, best_hyperparameters=hyperparameter_values)
