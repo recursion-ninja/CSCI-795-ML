ce6074eab29423db032d57325ecb94c8b539c06a
Alex Washburn <github@recursion.ninja>
2021-12-20 14:42:06 -0500

    Adding finalized parameters for KNN

--- classification/model_KNN.py
+++ classification/model_KNN.py
@@ -1,17 +1,22 @@
 from model_selection   import STATIC_SEED, model_evaluation
 from sklearn.neighbors import KNeighborsClassifier
 
-import numpy as np
-
 
 feature_extraction    = { 'tagged_trait'               : True
                         , 'standardized_label_classes' : 5
                         , 'decorrelate'                : 0.6
                         }
-search_grid_options   = { 'n_neighbors' : np.arange(1, 37, 2)
-                        , 'p'           : [1, 2, 5, 10, 20, 30, 50, 100]
-                        , 'weights'     : ["uniform", "distance"]
+search_grid_options   = { 'n_neighbors' : range(1, 23, 2) + range(23, 32) + range(33, 38, 2)
+                        , 'p'           : range(1,5)
+                        , 'weights'     : ['distance', 'uniform']
+                        , 'algorithm'   : ['auto', 'ball_tree', 'kd_tree']
+                        , 'leaf_size'   : range(1,10) + range(10, 51, 5)
                         }
-hyperparameter_values = None
+hyperparameter_values = { 'algorithm'   : 'ball_tree'
+                        , 'weights'     : 'distance'
+                        , 'n_neighbors' : 30
+                        , 'leaf_size'   : 5
+                        , 'p'           : 1
+                        }
 
 model_evaluation("K Nearest Neighbors", KNeighborsClassifier(), dataset_params=feature_extraction, param_grid=search_grid_options, best_hyperparameters=hyperparameter_values)
