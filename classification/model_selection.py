ad04ae74bea4204b2e82c7e96fb6a5d65af00390
Alex Washburn <github@recursion.ninja>
2021-12-20 13:19:16 -0500

    Formatting

--- classification/model_selection.py
+++ classification/model_selection.py
@@ -1,9 +1,9 @@
 import dataset_transforms as datum
 import numpy              as np
-import pandas            as pd
+import pandas             as pd
 
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn import metrics
+from sklearn                 import metrics
 
 
 # It is nice to remove the deprecation warnings.
