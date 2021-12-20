50bbebd7de1641b27403d7faf333dce56fd707ad
johnl11235 <31864579+johnl11235@users.noreply.github.com>
2021-12-20 13:08:41 -0500

    KNN config file
    K Nearest Neighbors classifier file, hyperparameter tuning left on auto

---
+++ model_KNN.py
@@ -0,0 +1,15 @@
+from model_selection     import STATIC_SEED, model_evaluation
+from sklearn.neighbors import KNeighborsClassifier
+
+
+feature_extraction    = { 'tagged_trait'               : True
+                        , 'standardized_label_classes' : 5
+                        , 'decorrelate'                : 0.6
+                        }
+search_grid_options   = { 'n_neighbors': np.arange(1,37, 2)
+                        , 'p': [1, 2, 5,  10, 20, 30, 50, 100]
+                        , 'weights': ["uniform", "distance"]
+                        }
+hyperparameter_values = None
+
+model_evaluation("K Nearest Neighbors", KNeighborsClassifier(), dataset_params=feature_extraction, param_grid=search_grid_options, best_hyperparameters=hyperparameter_values)
